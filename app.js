850e57fca050e088735777e2abaabcab26a8b39a
nipplelion <piolotorrico@yahoo.com>
2024-10-10 08:49:34 -0700

    feat: add styling

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const path = require("node:path");
 
 const app = express();
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -28,23 +31,13 @@ const messages = [
   },
 ];
 
+let id = 3;
+
 app.get("/", (req, res) => {
   res.render("index", {
     title: "Mini Messageboard",
     links: links,
     messages: messages,
-  });
-});
-
-app.get("/:id", (req, res) => {
-  const { id } = req.params;
-
-  const message = messages.find((message) => message.id == id);
-
-  res.render("messages/message-overview", {
-    title: "Mini Messageboard",
-    links: links,
-    message: message,
   });
 });
 
@@ -57,8 +50,26 @@ app.get("/new", (req, res) => {
 
 app.post("/new", (req, res) => {
   const { messageText, messageUser } = req.body;
-  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  messages.push({
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+    id: id,
+  });
+  id += 1;
   res.redirect("/");
+});
+
+app.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  const message = messages.find((message) => message.id == id);
+
+  res.render("messages/message-overview", {
+    title: "Mini Messageboard",
+    links: links,
+    message: message,
+  });
 });
 
 const PORT = 3000;
